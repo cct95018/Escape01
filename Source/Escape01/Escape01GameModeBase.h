6060cd0ad17a63ca94143cbd6ba9a211651c2060
Chi Tam <chi.tam.377@my.csun.edu>
2017-01-23 15:52:14 -0800

    Initial commit.

---
+++ Source/Escape01/Escape01GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "Escape01GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPE01_API AEscape01GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
