2d05b7fa9c35f0cadb1432be1b8d50f5e078ab83
Chi Tam <chi.tam.377@my.csun.edu>
2017-01-23 18:24:07 -0800

    Moving door frame

---
+++ Source/Escape01/PosReporter.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Escape01.h"
+#include "PosReporter.h"
+
+
+// Sets default values for this component's properties
+UPosReporter::UPosReporter()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPosReporter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	FString ObjName = GetOwner()->GetName();
+	FString ObjPos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("Pos report for %s at %s"), *ObjName, *ObjPos);
+	
+}
+
+
+// Called every frame
+void UPosReporter::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
